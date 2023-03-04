c41aba3969cf41178d8c83506ec35f09d9ff6dce
Vasilis <vlalogiannis@gmail.com>
2023-03-04 17:06:49 +0200

    animated main text, scrollbar

--- pages/First.vue
+++ pages/First.vue
@@ -1,11 +1,7 @@
 <template>
 <div class="outterPage">
-  <div class="flex p-20 w-full h-full">
-    <div class="inline-block">
-      <div class="writer-text">
-        Hello, I'm Vasilis Web Developer
-      </div>
-  </div>
+  <div>
+    First
   </div>
   <div class="absolute right-2 bottom-3">
     <img class="animate-bounce" src="/icons/DownArrow.svg" />
