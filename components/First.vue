ef006f1754347a6154c0b7fc384ee6e9acd156a4
Vasilis <vlalogiannis@gmail.com>
2023-03-11 14:47:54 +0200

    before 3d

--- components/First.vue
+++ components/First.vue
@@ -1,20 +1,25 @@
 <template>
-<div class="outterPage">
-  <div class="flex p-20 w-full h-full">
-    <div class="inline-block">
-      <div class="writer-text">
-        Hello, I'm Vasilis Web Developer
+<div class="outterPage p-20">
+    <ul class="h-40 writer-text">
+      <div class="text-first">
+        Hello,
       </div>
-  </div>
+      <div class="text-second">
+        I'm Vasilis,
+      </div>
+      <div class="text-third">
+        Web Developer
+      </div>
+    </ul>
   </div>
   <div class="absolute right-2 bottom-3">
     <img class="animate-bounce" src="/icons/DownArrow.svg" />
-  </div>
 </div>
 
 </template>
 
 <script setup>
+
 
 </script>
 
